b6347e3329c5f65ba26852d876c8eb0871f42f69
SACCO <jennyrae818@gmail.com>
2022-05-14 12:38:38 -0700

    base file structure and installs - structure from mini project

---
+++ index.js
@@ -0,0 +1,21 @@
+
+//boilerplate - done
+
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const PORT = process.env.port || 3001;
+const app = express();
+
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
